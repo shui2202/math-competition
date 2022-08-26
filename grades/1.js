d1c841c0addfca00e1108800b0af47d0c790fad2
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-26 15:18:13 -0700

    Update 1.js

--- grades/1.js
+++ grades/1.js
@@ -1,9 +1,13 @@
 var submit = document.querySelector(".start")
-
+var stuff = document.querySelector(".stuff")
 submit.addEventListener("click", function(){
     var num1 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
     var num2 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
     var question = `What is ${num1} + ${num2}`
     var answer = num1 + num2
     var textbox = document.createElement("input")
+    var text = document.createElement("h3")
+    text.innerText = question
+    stuff.appendChild(text)
+    stuff.appendChild(textbox)
 })
