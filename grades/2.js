fb53ba272b22a9ba9f883c7c79f77e1d2695b504
githubcoding12 <111909901+githubcoding12@users.noreply.github.com>
2022-08-27 11:11:40 -0700

    Update 2.js

--- grades/2.js
+++ grades/2.js
@@ -6,6 +6,10 @@ submit.addEventListener("click", function(){
     console.log("h")
     var num1 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
     var num2 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
+    while (num1 >= num2 ) {
+        var num1 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
+        var num2 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
+    }
     var question = `What is ${num1} - ${num2}`
     var answer = num1 - num2
     var textbox = document.createElement("input")
