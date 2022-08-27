0844ce5fdb8cba3e3f3e0d373d6fb3c7cef57108
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-27 11:45:19 -0700

    Update 10.js

--- grades/10.js
+++ grades/10.js
@@ -1,14 +1,54 @@
-
+function binomial(n, k) {
+    if ((typeof n !== 'number') || (typeof k !== 'number')) 
+ return false; 
+   var coeff = 1;
+   for (var x = n-k+1; x <= n; x++) coeff *= x;
+   for (x = 1; x <= k; x++) coeff /= x;
+   return coeff;
+}
 var submit = document.querySelector(".start")
 var stuff = document.querySelector(".stuff")
+var level = document.querySelector(".level")
+var count = 0
 submit.addEventListener("click", function(){
-    var num1 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
-    var num2 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
-    var question = `What is ${num1} + ${num2}`
-    var answer = num1 + num2
+    var num1 = Math.floor((Math.random() * (12 - 2 + 2)) + 2);
+    var num2 = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+    var answer = binomial(num1, num2)
+    console.log(answer)
+    var question = `Evaluate binomial coefficient, where n is ${num1}, and k is ${num2}`
     var textbox = document.createElement("input")
     var text = document.createElement("h3")
+    var submit1 = document.createElement("button")
     text.innerText = question
+    submit1.innerText = "Submit"
+    text.classList.add("paragraph-styling")
+    textbox.classList.add("input-style")
+    submit1.classList.add("input-style")
     stuff.appendChild(text)
     stuff.appendChild(textbox)
+    stuff.appendChild(submit1)
+    gameOver = false
+    submit1.addEventListener("click", function(){
+        while (gameOver == false) {
+            var input = textbox.value
+            if (input == answer) {
+                submit.innerText = "Next round"
+                submit.classList.add("input-style")
+                stuff.removeChild(text)
+                stuff.removeChild(textbox)
+                stuff.removeChild(submit1)
+                alert("You got it correct!")
+                count ++
+                level.innerText = `Level: ${count}`   
+            } else {
+                alert("You got it wrong")
+                var returnToHome = document.createElement("a")
+                returnToHome.innerText = "Return to homepage"
+                returnToHome.href = "/index.html"
+                returnToHome.classList.add("return")
+                stuff.appendChild(returnToHome)
+                gameOver = true
+            }
+        }
+    })
 })
