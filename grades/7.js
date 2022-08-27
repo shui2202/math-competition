1f1abdd4a9c61d35ce05c55fd86f4a580755336b
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-27 11:44:26 -0700

    Update 7.js

--- grades/7.js
+++ grades/7.js
@@ -1,14 +1,48 @@
-
 var submit = document.querySelector(".start")
 var stuff = document.querySelector(".stuff")
+var level = document.querySelector(".level")
+var count = 0
 submit.addEventListener("click", function(){
-    var num1 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
-    var num2 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
-    var question = `What is ${num1} + ${num2}`
-    var answer = num1 + num2
+    var radius = Math.floor((Math.random() * (7 - 2 + 2)) + 2);
+    var question = `What is the area of a circle with the radius of ${radius}`
+    var answer = (radius * radius) * 3.14
+    console.log(answer)
     var textbox = document.createElement("input")
     var text = document.createElement("h3")
+    var submit1 = document.createElement("button")
+    var circle_draw = document.createElement("div")
+    circle_draw.innerHTML= "<svg><circle cx=200 cy=80 r=50 stroke=blue stroke-width=4 fill=black/><svg/>"
     text.innerText = question
+    submit1.innerText = "Submit"
+    text.classList.add("paragraph-styling")
+    textbox.classList.add("input-style")
+    submit1.classList.add("input-style")
     stuff.appendChild(text)
     stuff.appendChild(textbox)
+    stuff.appendChild(submit1)
+    stuff.appendChild(circle_draw)
+    gameOver = false
+    submit1.addEventListener("click", function(){
+        while (gameOver == false) {
+            var input = textbox.value
+            if (input == answer) {
+                submit.innerText = "Next round"
+                submit.classList.add("input-style")
+                stuff.removeChild(text)
+                stuff.removeChild(textbox)
+                stuff.removeChild(submit1)
+                alert("You got it correct!")
+                count ++
+                level.innerText = `Level: ${count}`   
+            } else {
+                alert("You got it wrong")
+                var returnToHome = document.createElement("a")
+                returnToHome.innerText = "Return to homepage"
+                returnToHome.href = "/index.html"
+                returnToHome.classList.add("return")
+                stuff.appendChild(returnToHome)
+                gameOver = true
+            }
+        }
+    })
 })
