5926ac8cfcdcb53490207d2f6bc0df8b58fb3b34
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-27 11:44:44 -0700

    Update 8.js

--- grades/8.js
+++ grades/8.js
@@ -1,14 +1,47 @@
-
 var submit = document.querySelector(".start")
 var stuff = document.querySelector(".stuff")
+var level = document.querySelector(".level")
+var count = 0
 submit.addEventListener("click", function(){
-    var num1 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
-    var num2 = Math.floor((Math.random() * (10 - 0 + 0)) + 0);
-    var question = `What is ${num1} + ${num2}`
-    var answer = num1 + num2
+    var leg1 = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+    var leg2 = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+    var answer = Math.sqrt((leg1 * leg1) + (leg2 * leg2))
+    console.log(answer)
+    var question = `The a leg of a right triangle is ${leg1} and another leg is ${leg2}, find the hypontenuse: (Calculators allowed)`
     var textbox = document.createElement("input")
     var text = document.createElement("h3")
+    var submit1 = document.createElement("button")
     text.innerText = question
+    submit1.innerText = "Submit"
+    text.classList.add("paragraph-styling")
+    textbox.classList.add("input-style")
+    submit1.classList.add("input-style")
     stuff.appendChild(text)
     stuff.appendChild(textbox)
+    stuff.appendChild(submit1)
+    gameOver = false
+    submit1.addEventListener("click", function(){
+        while (gameOver == false) {
+            var input = textbox.value
+            if (input == answer) {
+                submit.innerText = "Next round"
+                submit.classList.add("input-style")
+                stuff.removeChild(text)
+                stuff.removeChild(textbox)
+                stuff.removeChild(submit1)
+                alert("You got it correct!")
+                count ++
+                level.innerText = `Level: ${count}`   
+            } else {
+                alert("You got it wrong")
+                var returnToHome = document.createElement("a")
+                returnToHome.innerText = "Return to homepage"
+                returnToHome.href = "/index.html"
+                returnToHome.classList.add("return")
+                stuff.appendChild(returnToHome)
+                gameOver = true
+            }
+        }
+    })
 })
+
