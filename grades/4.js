7f9279703899e92f60e3d8de2fa1bdf4754d231b
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-27 11:55:14 -0700

    Update 4.js

--- grades/4.js
+++ grades/4.js
@@ -1,57 +1,19 @@
-var submit = document.querySelector(".start")
-var stuff = document.querySelector(".stuff")
-var level = document.querySelector(".level")
-var count = 0
-submit.addEventListener("click", function(){
-    var num1 = Math.floor((Math.random() * (9 - 1 + 1)) + 1);
-    var num2 = Math.floor((Math.random() * (9 - 1 + 1)) + 1);
-    var num3 = Math.random()
-    if (num3 < 0.5){
-        var symbol = "*"
-        var answer = num1 * num2
-    } else {
-        var symbol = "/"
-        var answer = num1 / num2
-        while (Number.isInteger(answer) == false) {
-            var num1 = Math.floor((Math.random() * (9 - 1 + 1)) + 1);
-            var num2 = Math.floor((Math.random() * (9 - 1 + 1)) + 1);
-            var answer = num1 / num2
-        }
-    }
-    var question = `What is ${num1} ${symbol} ${num2}`
-    var textbox = document.createElement("input")
-    var text = document.createElement("h3")
-    var submit1 = document.createElement("button")
-    text.innerText = question
-    submit1.innerText = "Submit"
-    text.classList.add("paragraph-styling")
-    textbox.classList.add("input-style")
-    submit1.classList.add("input-style")
-    stuff.appendChild(text)
-    stuff.appendChild(textbox)
-    stuff.appendChild(submit1)
-    gameOver = false
-    submit1.addEventListener("click", function(){
-        while (gameOver == false) {
-            var input = textbox.value
-            if (input == answer) {
-                submit.innerText = "Next round"
-                submit.classList.add("input-style")
-                stuff.removeChild(text)
-                stuff.removeChild(textbox)
-                stuff.removeChild(submit1)
-                alert("You got it correct!")
-                count ++
-                level.innerText = `Level: ${count}`   
-            } else {
-                alert("You got it wrong")
-                var returnToHome = document.createElement("a")
-                returnToHome.innerText = "Return to homepage"
-                returnToHome.href = "/index.html"
-                returnToHome.classList.add("return")
-                stuff.appendChild(returnToHome)
-                gameOver = true
-            }
-        }
-    })
-})
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math Competition</title>
+    <link rel="stylesheet" href="grade.css">
+</head>
+<body>
+    <h1>4th Grade Math Quiz</h1>
+    <h2 class="level">Level: 0</h2>
+    <button class="start">Start</button>
+    <div class="stuff">
+
+    </div>
+    <script src="4.js"></script>
+</body>
+</html>
