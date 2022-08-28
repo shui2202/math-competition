16a7c67cba66a41200a19c8cb32ee1bc15dc8781
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-27 22:34:51 -0700

    Update 8.js

--- grades/8.js
+++ grades/8.js
@@ -1,7 +1,11 @@
 var submit = document.querySelector(".start")
 var stuff = document.querySelector(".stuff")
+var more = document.querySelector(".more")
 var level = document.querySelector(".level")
+var points = document.querySelector(".points")
 var count = 0
+var xp = 0
+
 submit.addEventListener("click", function(){
     submit.disabled = true
     var leg1 = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
@@ -26,11 +30,6 @@ submit.addEventListener("click", function(){
         while (gameOver == false) {
             submit.disabled = false
             var input = textbox.value
-            if (input == "") {
-                alert("Please submit an answer!")
-                submit.disabled = true
-                break
-            }
             if (input == answer) {
                 submit.innerText = "Next round"
                 submit.classList.add("input-style")
@@ -39,9 +38,12 @@ submit.addEventListener("click", function(){
                 stuff.removeChild(submit1)
                 alert("You got it correct!")
                 count ++
-                level.innerText = `Level: ${count}`   
+                level.innerText = `Level: ${count}` 
+                xp += 10  
             } else {
                 alert("You got it wrong")
+                localStorage.setItem("points", parseInt(localStorage.getItem("points")) + xp)
+                points.innerText = "Stats - " + localStorage.getItem("points") + " points"
                 var returnToHome = document.createElement("a")
                 returnToHome.innerText = "Return to homepage"
                 returnToHome.href = "/index.html"
@@ -52,4 +54,5 @@ submit.addEventListener("click", function(){
         }
     })
 })
+
 
