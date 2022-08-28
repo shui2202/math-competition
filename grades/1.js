1361f88cf5255f5d5a2811a0f021e5d322100e72
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-28 12:23:07 -0700

    Update 1.js

--- grades/1.js
+++ grades/1.js
@@ -47,10 +47,7 @@ submit.addEventListener("click", function(){
             } else {
                 localStorage.clear()
                 alert(`You got it wrong. The correct answer was ${answer}.`)
-                if(localStorage.getItem("points") == undefined){
-                    localStorage.setItem("points", xp)
 
-                } 
                 localStorage.setItem("points", parseInt(localStorage.getItem("points")) + xp)
                 
                 points.innerText = "Stats - " + localStorage.getItem("points") + " points"
