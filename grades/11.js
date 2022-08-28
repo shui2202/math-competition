643fe0997e762781e50c3f4cdd31c635cbb5b899
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-27 22:38:28 -0700

    Update 11.js

--- grades/11.js
+++ grades/11.js
@@ -1,7 +1,11 @@
 var submit = document.querySelector(".start")
 var stuff = document.querySelector(".stuff")
+var more = document.querySelector(".more")
 var level = document.querySelector(".level")
+var points = document.querySelector(".points")
 var count = 0
+var xp = 0
+
 submit.addEventListener("click", function(){
     submit.disabled = true
     var num1 = Math.floor((Math.random() * (360 - 0 + 0)) + 0);
@@ -24,11 +28,6 @@ submit.addEventListener("click", function(){
         while (gameOver == false) {
             submit.disabled = false
             var input = textbox.value
-            if (input == "") {
-                alert("Please submit an answer!")
-                submit.disabled = true
-                break
-            }
             if (input == answer) {
                 submit.innerText = "Next round"
                 submit.classList.add("input-style")
@@ -37,17 +36,16 @@ submit.addEventListener("click", function(){
                 stuff.removeChild(submit1)
                 alert("You got it correct!")
                 count ++
-                level.innerText = `Level: ${count}`   
+                level.innerText = `Level: ${count}`
+                xp += 10  
             } else {
                 alert("You got it wrong")
-                var returnToHome = document.createElement("a")
-                returnToHome.innerText = "Return to homepage"
-                returnToHome.href = "/index.html"
-                returnToHome.classList.add("return")
-                stuff.appendChild(returnToHome)
+                localStorage.setItem("points", parseInt(localStorage.getItem("points")) + xp)
+                points.innerText = "Stats - " + localStorage.getItem("points") + " points"
                 gameOver = true
             }
         }
     })
 })
+
 
