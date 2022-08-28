075cd2ad7dbb39892986196a7b372d84c0ce927f
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-28 12:13:00 -0700

    Update 12.js

--- grades/12.js
+++ grades/12.js
@@ -44,7 +44,12 @@ submit.addEventListener("click", function(){
                 xp += 10
             } else {
                 alert("You got it wrong")
+                if(localStorage.getItem("points") == undefined){
+                    localStorage.setItem("points", xp)
+
+                } 
                 localStorage.setItem("points", parseInt(localStorage.getItem("points")) + xp)
+                
                 points.innerText = "Stats - " + localStorage.getItem("points") + " points"
                 gameOver = true
                 submit1.disabled = true
@@ -52,4 +57,5 @@ submit.addEventListener("click", function(){
         }
     })
 })
+
 
