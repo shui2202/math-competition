34dac18a340dbb05e83d4800f99205d3ac24ef60
shui2202 <108314495+shui2202@users.noreply.github.com>
2022-08-28 12:24:55 -0700

    Update 5.js

--- grades/5.js
+++ grades/5.js
@@ -71,10 +71,7 @@ submit.addEventListener("click", function(){
                 xp += 10   
             } else {
                 alert("You got it wrong")
-                if(localStorage.getItem("points") == undefined){
-                    localStorage.setItem("points", xp)
 
-                } 
                 localStorage.setItem("points", parseInt(localStorage.getItem("points")) + xp)
                 
                 points.innerText = "Stats - " + localStorage.getItem("points") + " points"
